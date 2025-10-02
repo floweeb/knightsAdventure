5a771bffa687ac5e3aef05dbf196740a081451b8
floweeb <floydqaranja@gmail.com>
2025-10-02 13:19:22 +0300

    [fix] some _ to unused vars, and adding deffering call in physics process to avoid errors

--- scripts/next_level.gd
+++ scripts/next_level.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	var current_level = get_tree().current_scene.scene_file_path
 	var file_name = current_level.get_file()                  # "level_03.tscn"
 	var num_str = file_name.trim_suffix(".tscn").split("_")[1] # "03"
@@ -18,7 +18,8 @@ func _on_body_entered(body: Node2D) -> void:
 
 	# Check if the file exists before loading (to avoid crashes)
 	if ResourceLoader.exists(next_scene_path):
-		get_tree().change_scene_to_file(next_scene_path)
+		get_tree().call_deferred('change_scene_to_file', next_scene_path)
+		#get_tree().change_scene_to_file(next_scene_path)
 	else:
 		print("Next level not found:", next_scene_path)
 	
